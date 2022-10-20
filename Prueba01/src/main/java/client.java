b56324b7a70cd67a9e080c1047f7236707792bb3
Reinaldo Aparicio <jreinaldo.aparicio@gmail.com>
2022-10-20 19:31:16 -0400

    add from intellij

---
+++ Prueba01/src/main/java/client.java
@@ -0,0 +1,16 @@
+public class client {
+    public static void main (String []asdad){
+
+        int a=9;
+        int b=10;
+
+        if (a>b){
+            a=a*2;
+            System.out.println(a);
+        }else{
+            b=b*2;
+            System.out.println(b);
+        }
+    }
+
+}
